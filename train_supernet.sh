4e61e2cfff3a26b3ad1e7caabc665ffe4b5fdb26
xiteng01 <xiteng01@baidu.com>
2022-04-07 21:46:34 +0800

    add train_suernet.sh

---
+++ train_supernet.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train_supernet.py run \
+  --backbone resnet48 \
+  --max_epoch 70 \
+  --batch_size 256 \
+  --lr 0.001 \
+  --warmup 5 \
+  --dyna_batch_size 2 \
+  --pretrained checkpoints/resnet48.pdparams \
+  --save_dir checkpoints/res48-depth \
+  --log_freq 1 \
